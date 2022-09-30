e0e3e526740a0b2d2e328f90987db4249088553b
Willedge <weihong.will@gmail.com>
2022-09-30 20:50:15 +0800

    Article.js is model, article.js is for hold api

---
+++ models/Article.js
@@ -0,0 +1,38 @@
+const mongoose = reuire("mongoose");
+
+const ArticleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+      },
+      author: {
+        type: String,
+        required: true,
+      },
+      source: {
+        type: String,
+      },
+      year: {
+        type: String,
+        required: true,
+      },
+      doi: {
+        type: String,
+      },
+      practice: {
+        type: String,
+        required: true,
+      },
+      claimed: {
+        type: String,
+        required: true,
+      },
+      evidence: {
+        type: String,
+      },
+      state: {
+        type: String,
+      }, 
+});
+
+module.exports = Article = mongosse.model("article", ArticleSchema);
